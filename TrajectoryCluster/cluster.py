c0a39892afde6d1472d469a279d6cfef04e223f0
WangJun <2858600511@qq.com>
2021-04-08 10:39:25 +0800

    Douglas-Peucker算法已初步完成，但是航迹数量还是太多了，另外D-P算法的阈值还需要再想办法确定。

--- TrajectoryCluster/cluster.py
+++ TrajectoryCluster/cluster.py
@@ -5,6 +5,7 @@ from sqlalchemy import and_
 from sqlalchemy import func
 from TrajectoryCluster.trajectory import Trajectory
 import datetime
+import matplotlib.pyplot as plt
 
 engine = create_engine("mysql+pymysql://root:123456@localhost:3306/ais?charset=utf8")
 # 创建session
@@ -21,8 +22,13 @@ session = DbSession()
 
 # 读取研究范围内所有航速大于1的AIS点
 datas = session.query(AIS).filter(
-    and_(AIS.LAT >= 33.5, AIS.LAT <= 33.6, AIS.LON >= -118.35, AIS.LON <= -118.25, AIS.SOG >= 1)).order_by(AIS.MMSI,
-                                                                                                           AIS.BaseDateTime).all()
+    and_(AIS.LAT >= 33.5,
+         AIS.LAT <= 33.6,
+         AIS.LON >= -118.35,
+         AIS.LON <= -118.25,
+         AIS.SOG >= 1,
+         AIS.BaseDateTime >= '2017-01-01',
+         AIS.BaseDateTime <= '2017-07-01')).order_by(AIS.MMSI, AIS.BaseDateTime).all()
 # 将航迹依据MMSI分开
 # 将间隔时间大于10min的轨迹断开
 MMSI = datas[0].MMSI
@@ -30,13 +36,52 @@ trajectories = []
 trajectory = Trajectory(MMSI)
 for data in datas:
     if data.MMSI == MMSI:
-        if trajectory.getLength() == 0 or (data.BaseDateTime - trajectory.points[trajectory.getLength() - 1].BaseDateTime).seconds <= 600:
+        if trajectory.getLength() == 0 or \
+                (data.BaseDateTime - trajectory.points[trajectory.getLength() - 1].BaseDateTime).seconds <= 600:
             trajectory.add_point(data)
             continue
-    trajectories.append(trajectory)
+    if len(trajectory.points) > 10:
+        trajectories.append(trajectory)
     MMSI = data.MMSI
     trajectory = Trajectory(MMSI)
     trajectory.add_point(data)
+
+
+def drawTrajectory():
+    global trajectory
+    fig = plt.figure()
+    a1 = fig.add_subplot(111)
+    for trajectory in trajectories:
+        dx = []
+        dy = []
+        for i in range(trajectory.getLength()):
+            dx.append(trajectory.points[i].LON)
+            dy.append(trajectory.points[i].LAT)
+        a1.plot(dx, dy, color='r', linestyle='-', marker='+')
+        dx.clear()
+        dy.clear()
+    plt.show()
+
+
+# 航迹压缩前画图
+drawTrajectory()
+
+# 航迹压缩前共存在AIS点个数，以及总航程
+aisNumBefore = 0
+for trajectory in trajectories:
+    aisNumBefore += len(trajectory.points)
+
+# 航迹压缩
+for trajectory in trajectories:
+    trajectory.compress(trajectory.points[0], trajectory.points[trajectory.count-1])
+
+# 航迹压缩后共存在AIS点个数，以及总航程
+aisNumAfter = 0
+for trajectory in trajectories:
+    aisNumAfter += len(trajectory.points)
+
+# 航迹压缩后画图
+drawTrajectory()
 
 print(len(datas))
 
--- TrajectoryCluster/trajectory.py
+++ TrajectoryCluster/trajectory.py
@@ -1,7 +1,10 @@
+import math
 class Trajectory:
     points = []
     count = 0
     MMSI = "null"
+    D = 0.0015  # 航迹压缩算法的容差
+    index = []  # 用于表示AIS点在轨迹上的索引
 
     def add_point(self, point):
         self.points.append(point)
@@ -12,6 +15,31 @@ class Trajectory:
 
     def getLength(self):
         return len(self.points)
+
+    # 轨迹压缩
+    def compress(self, p1, p2):
+        self.index = list(range(self.count))
+        left = self.points.index(p1)
+        right = self.points.index(p2)
+        if right == left+1:
+            return
+        A = p1.LAT - p2.LAT
+        B = p2.LON - p1.LON
+        C = p1.LON*p2.LAT - p2.LON*p1.LAT
+        distance = []
+        # 计算中点到直线的距离
+        for i in range(left+1, right):
+            d = abs(A*self.points[i].LON + B*self.points[i].LAT + C) / math.sqrt(math.pow(A, 2) + math.pow(B, 2))
+            distance.append(d)
+        dmax = max(distance)
+        if dmax <= self.D:
+            for i in range(right-left-1):
+                del self.points[left+1]
+        else:
+            # middle表示距离最远的点
+            middle = self.points[distance.index(dmax) + left + 1]
+            self.compress(p1, middle)
+            self.compress(middle, p2)
 
     def __init__(self, MMSI):
         self.MMSI = MMSI
